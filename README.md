12e9cc89238a7fca8f93514f4ad45b4c41a734aa
StudiousXiaoYu <1316356098@qq.com>
2024-08-08 17:42:30 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # python-tidb-chat2sql
 利用Python+tidb向量数据库制作文生SQL功能
+
+
+# LlamaIndex RAG Example with Simple UI
+
+This example demonstrates how to use the LlamaIndex and TiDB Serverless to build a simple RAG(Retrival-Augmented Generation) application with simple UI.
+
+## Prerequisites
+
+- A running TiDB Serverless cluster with vector search enabled
+- Python 3.8 or later
+- OpenAI [API key](https://platform.openai.com/docs/quickstart)
+
+## Run the example
+
+### Clone this repo
+
+```bash
+git clone https://github.com/pingcap/tidb-vector-python.git
+```
+
+### Create a virtual environment
+
+```bash
+cd tidb-vector-python/examples/llamaindex-tidb-vector-with-ui
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+### Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+### Set the environment variables
+
+Get the `OPENAI_API_KEY` from [OpenAI](https://platform.openai.com/docs/quickstart)
+
+Get the `TIDB_HOST`, `TIDB_USERNAME`, and `TIDB_PASSWORD` from the TiDB Cloud console, as described in the [Prerequisites](../README.md#prerequisites) section.
+
+```bash
+export OPENAI_API_KEY="sk-*******"
+export TIDB_HOST="gateway01.*******.shared.aws.tidbcloud.com"
+export TIDB_USERNAME="****.root"
+export TIDB_PASSWORD="****"
+```
+
+### Prepare data and run the server
+
+```bash
+# prepare the data
+python app.py prepare
+
+# runserver
+python app.py runserver
+```
+
+Now you can visit [http://127.0.0.1:3000/](http://127.0.0.1:3000/) to interact with the RAG application.
---
+++ app.py
@@ -0,0 +1,116 @@
+import os
+import sys
+import json
+import logging
+import click
+import uvicorn
+import fastapi
+import asyncio
+from enum import Enum
+from sqlalchemy import URL
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import StreamingResponse, HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from llama_index.core import VectorStoreIndex, StorageContext
+from llama_index.core.base.response.schema import StreamingResponse as llamaStreamingResponse
+from llama_index.vector_stores.tidbvector import TiDBVectorStore
+from llama_index.readers.web import SimpleWebPageReader
+
+# 设置环境变量
+os.environ['OPENAI_API_KEY'] = 'sk-*******'
+os.environ['TIDB_HOST'] = 'gateway01.*******.shared.aws.tidbcloud.com'
+os.environ['TIDB_USERNAME'] = '****.root'
+os.environ['TIDB_PASSWORD'] = '****'
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger()
+
+
+class EventType(Enum):
+    META = 1
+    ANSWER = 2
+
+
+logger.info("Initializing TiDB Vector Store....")
+tidb_connection_url = URL(
+    "mysql+pymysql",
+    username=os.environ['TIDB_USERNAME'],
+    password=os.environ['TIDB_PASSWORD'],
+    host=os.environ['TIDB_HOST'],
+    port=4000,
+    database="test",
+    query={"ssl_verify_cert": True, "ssl_verify_identity": True},
+)
+tidbvec = TiDBVectorStore(
+    connection_string=tidb_connection_url,
+    table_name="llama_index_rag_test",
+    distance_strategy="cosine",
+    vector_dimension=1536, # Length of the vectors returned by the model
+    drop_existing_table=False,
+)
+tidb_vec_index = VectorStoreIndex.from_vector_store(tidbvec)
+storage_context = StorageContext.from_defaults(vector_store=tidbvec)
+query_engine = tidb_vec_index.as_query_engine(streaming=True)
+logger.info("TiDB Vector Store initialized successfully")
+
+
+def do_prepare_data():
+    logger.info("Preparing the data for the application")
+    documents = SimpleWebPageReader(html_to_text=True).load_data(
+        ["http://paulgraham.com/worked.html"]
+    )
+    tidb_vec_index.from_documents(documents, storage_context=storage_context, show_progress=True)
+    logger.info("Data preparation complete")
+
+
+# https://stackoverflow.com/questions/76288582/is-there-a-way-to-stream-output-in-fastapi-from-the-response-i-get-from-llama-in
+async def astreamer(response: llamaStreamingResponse):
+    try:
+        meta = json.dumps(jsonable_encoder(list(vars(node) for node in response.source_nodes)))
+        yield f'{EventType.META.value}: {meta}\n\n'
+        for i in response.response_gen:
+            yield f'{EventType.ANSWER.value}: {i}\n\n'
+            await asyncio.sleep(.1)
+    except asyncio.CancelledError as e:
+        print('cancelled')
+
+
+app = fastapi.FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: fastapi.Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get('/ask')
+async def ask(q: str):
+    response = query_engine.query(q)
+    return StreamingResponse(astreamer(response), media_type='text/event-stream')
+
+
+@click.group(context_settings={'max_content_width': 150})
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--host', default='127.0.0.1', help="Host, default=127.0.0.1")
+@click.option('--port', default=3000, help="Port, default=3000")
+@click.option('--reload', is_flag=True, help="Enable auto-reload")
+def runserver(host, port, reload):
+    uvicorn.run(
+        "__main__:app", host=host, port=port, reload=reload,
+        log_level="debug", workers=1,
+    )
+
+
+@cli.command()
+def prepare():
+    do_prepare_data()
+
+
+if __name__ == '__main__':
+    cli()
